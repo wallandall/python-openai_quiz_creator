04cd2f7bf51cce29346dfab91c6fdadc82fa2028
wallandall <wallandall@gmail.com>
2023-04-25 22:29:38 +0200

    Initial Commit

---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+# Python OpenAI Quiz Creator
+
+This progrogram uses Pythen and OPENAI to generate a list of questions and answers based on a topic entered via a prompt. The program provides the option for generating quizes as well as an option to answering a generated quiz and providing feedback with the grades of the quiz
+
+## Setup
+
+1. Run `Python -m pip install -r requirements.txt`
+2. Create a ___.env___ file to store your API Key. The contents of the file should be as below:
+  
+```Python
+API_SECRET='ENTER_YOUR_KEY'
+```
+
+## Running the Application
+
+1. Activate the Virtual Environment by typing :
+   1. On Windows: `.\venv\Scripts\activate`
+   2. On Linux: `source tutorial-env/bin/activate`
+
+2. Type : `Python main.py`
---
+++ teacher.py
@@ -0,0 +1,61 @@
+import logging
+
+from test_creator import TestGenerator
+
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
+
+
+
+class Teacher:
+    def __init__(self):
+        print("Welcome! Run create_full_test() to create a test.")
+
+
+    def create_full_test(self):
+
+        topic = input("What topic would you like to create a test on? ")
+        num_possible_answers = int(input("How many possible answers would you like to have? "))
+        num_questions = int(input("How many possible answers per question would you like to have? "))
+
+        self.test_creator = TestGenerator(topic, num_possible_answers, num_questions)
+        test = self.test_creator.run()
+    
+        #logging.info(test)
+        student_view = self.create_student_view(test, num_questions)
+        answers = self.extract_answers(test, num_questions)
+        return student_view, answers
+
+    def create_student_view(self, test, num_questions):
+        student_view = {1 : ""}
+        question_number = 1
+        for line in test.split("\n"):
+            if not line.startswith("Correct Answer:"):
+                student_view[question_number] += line+"\n"
+            else:
+                
+                if question_number < num_questions:
+                    question_number+=1
+                    student_view[question_number] = ""
+        return student_view
+
+    def extract_answers(self, test, num_questions):
+        answers = {1 : ""}
+        question_number = 1
+        for line in test.split("\n"):
+            if line.startswith("Correct Answer:"):
+                answers[question_number] += line+"\n"
+
+                if question_number < num_questions:
+                    question_number+=1
+                    answers[question_number] = ""
+        return answers
+
+        
+
+if __name__ == "__main__":
+    teacher = Teacher()
+    student_view, answers = teacher.create_full_test()
+    print(student_view)
+    print(answers)
